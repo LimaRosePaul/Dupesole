7b5805b6e2b97ac9ee4ab8dd33f47c9b79405548
Lima Rose Paul <limarosepaul1@gmail.com>
2024-05-10 13:03:44 +0530

    first commit

---
+++ e-commerce-frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './RelatedProducts.css';
+import Item from '../Item/Item';
+import data_product from '../Assets/data';
+
+const YourComponent = () => {
+  return (
+    <div className='your-component'>
+      {/* Content of your component */}
+    </div>
+  );
+}
+
+export default YourComponent;
